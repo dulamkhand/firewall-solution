73c402eea262ec36c4073317b6ffd86a6c22b34e
handaa.1224@gmail.com <handaa.1224@gmail.com@767ca357-fd03-d4fb-bd5b-b5f542474f7a>
2014-07-05 09:54:59 +0000

    work

--- trunk/apps/front/modules/main/templates/_feedback.php
+++ trunk/apps/front/modules/main/templates/_feedback.php
@@ -1,56 +1,72 @@
-<form action="<?php echo url_for('main/contact')?>" method="post" id="formFeedback">
-    <h2 style="margin:0 0 10px 0;font-family:'Roboto Condensed';font-size:20px;color:#177B01;text-align:center;width:95%;">
-        <?php echo __('Leave us a message')?>
-    </h2>
-
-    <br clear="all">
-    <div class="left" style="margin:0;width:380px;text-align:right;">
-        <?php echo $form['name']->renderError() ?>
-        <?php echo $form['name'] ?>
-      
-        <?php echo $form['email']->renderError() ?>
-        <?php echo $form['email'] ?>
-      
-        <?php echo $form['phone']->renderError() ?>
-        <?php echo $form['phone'] ?>
-    </div>
-	
-    <div class="left" style="margin:0 0 0 1px;width:390px;text-align:left;">
-        <?php echo $form['message']->renderError() ?>
-        <?php echo $form['message'] ?>
-        <br clear="all">
-        <a href="javascript:$('#formFeedback').submit();" style="margin:0 0 0 -17px;font-size:15px;color:#177B01;">
-            <?php echo __('Send')?>
-        </a>
-    </div>
+<form action="<?php echo url_for('main/index')?>" method="post" id="formFeedback">
+    <table width="100%">
+      <tr>
+          <td>
+              <?php echo $form['organization']->renderError() ?>
+              <?php echo $form['organization'] ?>
+          </td>
+          <td>
+              <?php echo $form['name']->renderError() ?>
+              <?php echo $form['name'] ?>
+          </td>
+      </tr>
+      <tr>
+          <td>
+              <?php echo $form['email']->renderError() ?>
+              <?php echo $form['email'] ?>
+          </td>
+          <td>
+              <?php echo $form['phone']->renderError() ?>
+              <?php echo $form['phone'] ?>
+          </td>
+      </tr>
+      <tr>
+          <td colspan="2">
+              <?php echo $form['message']->renderError() ?>
+              <?php echo $form['message'] ?>
+          </td>
+      </tr>
+      <tr>
+         <td colspan="2">
+            <a href="javascript:$('#formFeedback').submit();" class="button"><?php echo __('SEND')?></a>
+         </td>
+      </tr>
+    </table>
     <br clear="all">
 </form>
 
 
 <script type="text/javascript">
 $(document).ready(function(){
-    $('#feedback_name').click(function(){
-        if($(this).val() == '<?php echo __('Your name')?>') $(this).val('');
-    }).blur(function(){
-        if($(this).val() == '') $(this).val('<?php echo __('Your name')?>');
-    });
-    
-    $('#feedback_email').click(function(){
-        if($(this).val() == '<?php echo __('Email')?>') $(this).val('');
-    }).blur(function(){
-        if($(this).val() == '') $(this).val('<?php echo __('Email')?>');
-    });
-    
-    $('#feedback_phone').click(function(){
-        if($(this).val() == '<?php echo __('Phone')?>') $(this).val('');
-    }).blur(function(){
-        if($(this).val() == '') $(this).val('<?php echo __('Phone')?>');
-    });
-    
-    $('#feedback_message').click(function(){
-        if($(this).val() == '<?php echo __('Message')?>') $(this).val('');
-    }).blur(function(){
-        if($(this).val() == '') $(this).val('<?php echo __('Message')?>');
-    });
+  $('#feedback_organization').click(function(){
+      if($(this).val() == '<?php echo __('Organization')?>') $(this).val('');
+  }).blur(function(){
+      if($(this).val() == '') $(this).val('<?php echo __('Organization')?>');
+  });
+
+  $('#feedback_name').click(function(){
+      if($(this).val() == '<?php echo __('Your name')?>') $(this).val('');
+  }).blur(function(){
+      if($(this).val() == '') $(this).val('<?php echo __('Your name')?>');
+  });
+  
+  $('#feedback_email').click(function(){
+      if($(this).val() == '<?php echo __('Email')?>') $(this).val('');
+  }).blur(function(){
+      if($(this).val() == '') $(this).val('<?php echo __('Email')?>');
+  });
+  
+  $('#feedback_phone').click(function(){
+      if($(this).val() == '<?php echo __('Phone')?>') $(this).val('');
+  }).blur(function(){
+      if($(this).val() == '') $(this).val('<?php echo __('Phone')?>');
+  });
+  
+  $('#feedback_message').click(function(){
+      if($(this).val() == '<?php echo __('About product')?>') $(this).val('');
+  }).blur(function(){
+      if($(this).val() == '') $(this).val('<?php echo __('About product')?>');
+  });
+
 });
 </script>
--- trunk/apps/front/templates/_header.php
+++ trunk/apps/front/templates/_header.php
@@ -1,25 +1,33 @@
 <div id="header">
+<div class="wrapper">
 <div id="menu">
     <?php $action = $sf_request->getParameter('action')?>
     <ul>
-        <li class="<?php if($action == 'index') echo 'current'?>">
-           <a href="<?php echo url_for('@homepage')?>"><?php echo image_tag('logo-tr.png', array('style'=>'float:left;margin:-45px 0 0 -35px;'))?></a>
+        <li>
+           <a href="<?php echo url_for('@homepage')?>" style="margin:0;">
+              <?php echo image_tag('logo-tr.png', array('style'=>'float:left;margin:-32px 0 0 -5px;'))?>
+          </a>
         </li>
         <li class="<?php if($action == 'about') echo 'current'?>">
-           <a href="<?php echo url_for('main/about')?>"><?php echo __('About Us')?></a>
+           <a href="<?php echo url_for('main/about')?>"><?php echo __('Товч танилцуулга')?></a>
         </li>
-        <li class="<?php if($action == 'about') echo 'current'?>">
-           <a href="<?php echo url_for('main/about')?>"><?php echo __('Our success')?></a>
-        </li>
+        <li class="separator"></li>
         <li class="<?php if(in_array($action, array('products', 'productShow'))) echo 'current'?>">
-           <a href="<?php echo url_for('content/products')?>"><?php echo __('Our service')?></a>
+           <a href="<?php echo url_for('content/products')?>"><?php echo __('Бүтээгдэхүүн үйлчилгээ')?></a>
         </li>
+        <li class="separator"></li>
+        <li class="<?php if($action == 'jobs') echo 'current'?>">
+           <a href="<?php echo url_for('main/about')?>"><?php echo __('Хийж гүйцэтгэсэн ажлууд')?></a>
+        </li>
+        <li class="separator"></li>
+        <li class="<?php if($action == 'clients') echo 'current'?>">
+           <a href="<?php echo url_for('main/clients')?>"><?php echo __('Харилцагч байгууллагууд')?></a>
+        </li>
+        <li class="separator"></li>
         <li class="<?php if($action == 'contact') echo 'current'?>">
            <a href="<?php echo url_for('main/contact')?>"><?php echo __('Contact')?></a>
         </li>
     </ul>
 </div>
 </div>
-
-<br clear="all">
-<br clear="all">
+</div>
