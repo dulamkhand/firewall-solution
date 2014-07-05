ed325dbabef151a9824d3e212f7c859535f1866d
handaa.1224@gmail.com <handaa.1224@gmail.com@767ca357-fd03-d4fb-bd5b-b5f542474f7a>
2014-07-05 10:58:13 +0000

    work

---
+++ apps/front/modules/main/templates/_jobs.php
@@ -0,0 +1,28 @@
+<div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/h.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/d.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/e.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/g.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/a.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/f.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/c.jpg', array())?>
+    </div>
+    <div class="left" style="height:350px;margin:0 13px 13px 0;padding:0;background:#F3F3F3;">
+        <?php echo image_tag('/u/jobs/i.jpg', array())?>
+    </div>
+    <br clear="all">
+</div>
+
