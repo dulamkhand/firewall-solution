f15f2b0bc5571cbc3197d18492d636a00789131d
handaa.1224@gmail.com <handaa.1224@gmail.com@767ca357-fd03-d4fb-bd5b-b5f542474f7a>
2014-07-16 15:30:46 +0000

    10 - broshure

---
+++ trunk/apps/front/modules/content/templates/broshureSuccess.php
@@ -0,0 +1,39 @@
+﻿<div class="wrapper" style="width:1300px;">
+
+<?php echo image_tag('logo.png', array())?>
+
+<br clear="all">
+<div style="float:left;width:390px;height:270px;padding:15px;border-top-left-radius:6px;border-bottom-left-radius:6px;background:#;" class="bg-orange">
+	
+</div>
+	
+<div style="float:left;width:390px;height:270px;padding:15px;background:#0089B9;" class="bg-blue">
+		<h2 style="color:#fff;">Товч танилцуулга</h2>
+		йлыобрх лйрба рхЫАӨРа йлырайлы ЙЫРХАЙЫЛРХ 
+		1 йлыобрх лйрба рхЫАӨРа йлырайлы ЙЫРХАЙЫЛРХ 
+		йлр йлыобрх лйрба рхЫАӨРа йлырайлы ЙЫРХАЙЫЛРХ 
+		йлр С йлыобрх лйрба рхЫАӨРа йлырайлы ЙЫРХАЙЫЛРХ 
+		йлр йлр
+</div>
+
+<div style="float:left;width:390px;height:270px;padding:15px;;" class="bg-orange">
+<h2 style="color:#fff;">Товч танилцуулга</h2>
+		- 2012 asljdhg asdlgjasdgl
+		- 2013 lasdjgasldkgjh
+		- 2014 sakjhgaskjh
+		1 йлы
+</div>
+
+<div style="float:left;width:390px;height:270px;padding:15px;" class="bg-orange">
+
+</div>
+
+<div style="float:left;width:390px;height:270px;padding:15px;border-top-right-radius:6px;border-bottom-right-radius:6px;background:#;" class="bg-white">
+
+</div>
+<div style="float:left;width:390px;height:270px;padding:15px;" class="bg-yellow">
+
+</div>
+
+
+</div>
