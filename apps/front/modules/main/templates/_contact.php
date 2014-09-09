2b8bc75ada2531598b9f8509596843347488e351
handaa.1224@gmail.com <handaa.1224@gmail.com@767ca357-fd03-d4fb-bd5b-b5f542474f7a>
2014-09-09 01:49:50 +0000

    work

--- apps/front/modules/main/templates/_contact.php
+++ apps/front/modules/main/templates/_contact.php
@@ -17,38 +17,4 @@
 <br clear="all">
 <hr style="border:0;border-top:1px solid #dedede;margin:10px 50px 20px 0;">
 
-
-<span style="text-decoration:none;">
-    <?php echo sfConfig::get('app_phone')?>   
-</span>
-<br clear="all">
-<a href="mailto:<?php echo sfConfig::get('app_email')?>" style="text-decoration:none;">
-   <?php echo sfConfig::get('app_email')?>
-</a><br>
-<br clear="all">
-
-<span style="text-decoration:none;">
-    <?php echo $sf_user->getCulture() == 'en' ? sfConfig::get('app_address_en') 
-        : sfConfig::get('app_address')?>
-</span>
-<br clear="all"/>
-<br clear="all"/>
-
-<a href="http://www.facebook.com/<?php echo sfConfig::get('app_facebook')?>" target="_blank" style="text-decoration:none;">
-   <?php echo image_tag('i/facebook-4-m.png', array())?>
-</a>
-<a href="http://www.twitter.com/<?php echo sfConfig::get('app_twitter')?>" target="_blank" style="text-decoration:none;">
-   <?php echo image_tag('i/twitter-4-m.png', array())?>
-</a>
-<a href="http://www.youtube.com/<?php echo sfConfig::get('app_youtube')?>" target="_blank" style="text-decoration:none;">
-   <?php echo image_tag('i/youtube-4-m.png', array())?>
-</a>
-<a href="skype:<?php echo sfConfig::get('app_skype')?>?call" target="_blank" style="text-decoration:none;">
-   <?php echo image_tag('i/skype-4-m.png', array())?>
-</a>
-<a href="http://www.pinterest.com/<?php echo sfConfig::get('app_pinterest')?>" target="_blank" style="text-decoration:none;">
-   <?php echo image_tag('i/pinterest-4-m.png', array())?>
-</a>
-<a href="http://www.instagram.com/<?php echo sfConfig::get('app_instagram')?>" target="_blank" style="text-decoration:none;">
-   <?php echo image_tag('i/instagram-4-m.png', array())?>
-</a>
+<?php include_partial('main/contactText', array());?>
