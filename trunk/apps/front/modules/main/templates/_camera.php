ef2c3d083c517d7c2ce994c577ed473b3820acbf
handaa.1224@gmail.com <handaa.1224@gmail.com@767ca357-fd03-d4fb-bd5b-b5f542474f7a>
2014-09-12 09:55:31 +0000

    work

--- trunk/apps/front/modules/main/templates/_camera.php
+++ trunk/apps/front/modules/main/templates/_camera.php
@@ -1,37 +1,40 @@
 <div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 1')?></h3>
         <?php echo image_tag('/u/camera/1.png', array('style'=>'max-width:315px;max-height:270px'))?>
+        <div class="trans">Албан байгууллага, сургууль, цэцэрлэгт тохиромжтой</div>
     </div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 2')?></h3>
         <?php echo image_tag('/u/camera/2.png', array('style'=>'max-width:315px;max-height:270px'))?>
+        <div class="trans">Хүнсний дэлгүүр, аптек, ломбард, жижиг хэмжээний обьектэд тохиромжтой</div>
     </div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 3')?></h3>
         <?php echo image_tag('/u/camera/3.png', array('style'=>'max-width:315px;max-height:270px'))?>
+        <div class="trans">Мөнгөн тэмдэгт таних чадвартай камерын багц, банк, валют арилжааны газруудад тохиромжтой</div>
     </div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 4')?></h3>
         <?php echo image_tag('/u/camera/4.png', array('style'=>'max-width:315px;max-height:270px'))?>
     </div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 5')?></h3>
         <?php echo image_tag('/u/camera/5.png', array('style'=>'max-width:315px;max-height:270px'))?>
     </div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 6')?></h3>
         <?php echo image_tag('/u/camera/6.png', array('style'=>'max-width:315px;max-height:270px'))?>
     </div>
-    <div class="left camera-box">
+    <div class="camera-box">
 		    <h3><?php echo __('Package 7')?></h3>
         <?php echo image_tag('/u/camera/8.png', array('style'=>'max-width:315px;max-height:270px'))?>
     </div>
-	 <div class="left camera-box">
+	 <div class="camera-box">
 		    <h3><?php echo __('Package 8')?></h3>
         <?php echo image_tag('/u/camera/9.png', array('style'=>'max-width:315px;max-height:270px'))?>
     </div>
-	 <div class="left camera-box">
+	 <div class="camera-box">
 		    <h3><?php echo __('Your idea?')?></h3>
     </div>
 </div>
